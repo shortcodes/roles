ca2818ac053f3b54fd51c137a16fe1916dbe6018
shortcodes <r.szymanski@mindz.it>
2020-03-06 09:59:27 +0100

    Update AccessMiddleware.php
    fix() - manage multiroles

--- src/Middleware/AccessMiddleware.php
+++ src/Middleware/AccessMiddleware.php
@@ -7,9 +7,19 @@ use Closure;
 
 class AccessMiddleware
 {
-    public function handle($request, Closure $next, $role)
+   public function handle($request, Closure $next, $role)
     {
-        if (!Auth::user()->is($role) && !in_array(Auth::user()->email, config('access.admins'))) {
+        $roles = array_except(func_get_args(), [0,1]);
+
+        $userHasAccess = false;
+
+        foreach ($roles as $role) {
+            if (Auth::user()->is($role)) {
+                $userHasAccess = true;
+            }
+        }
+
+        if (!$userHasAccess && !in_array(Auth::user()->email, config('access.admins'))) {
             return response()->json(['message' => 'You don\'t have permission to perform this action'], 403);
         }
 
